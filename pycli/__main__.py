69d215ea3abc96e43c393f8c60256f6ebda63b82
Vilém Janota <willjanota@gmail.com>
2024-06-10 10:23:59 +0200

    add save function

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -1,8 +1,22 @@
 import sys
+from .save import save
+
+def print_usage():
+    print("Usage: img-cli <command> <options>")
+    print("Commands:")
+    print("save <filename> - Save an image from clipboard")
+
 
 def main():
-    print("Hello World")
-
+    args = sys.argv[1:]
+    if not args:
+        print_usage()
+        return
+    if args[0] == 'save':
+        if len(args) < 2:
+            print("Error: save command requires a filename")
+            return
+        save(args[1])
 
 if __name__ == '__main__':
     main()
---
+++ pycli/save.py
@@ -0,0 +1,6 @@
+import os
+
+def save(output_file):
+    print("saving to", output_file + "...")
+    if(os.system(f"xclip -selection clipboard -target image/png -out > ./{output_file} 2>/dev/null")!=0):
+        print("Clipboard does not contain an image")
