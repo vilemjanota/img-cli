3367b641c107fa7cd0b484a2e7d8a586533e95fd
Vilém Janota <willjanota@gmail.com>
2024-06-10 17:08:44 +0200

    add docs

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -2,12 +2,18 @@ import sys
 from .save import save
 
 def print_usage():
+    """
+    Prints the usage instructions for the command-line interface.
+    """
     print("Usage: img-cli <command> <options>")
     print("Commands:")
     print("save <filename> - Save an image from clipboard")
 
-
 def main():
+    """
+    Entry point of the command-line interface.
+    Parses the command-line arguments and executes the corresponding command.
+    """
     args = sys.argv[1:]
     if not args:
         print_usage()
--- pycli/save.py
+++ pycli/save.py
@@ -1,6 +1,15 @@
 import os
 
 def save(output_file):
+    """
+    Save an image from the clipboard to a file.
+
+    Args:
+        output_file (str): The name of the output file.
+
+    Returns:
+        None
+    """
     print("saving to", output_file + "...")
     if(os.system(f"xclip -selection clipboard -target image/png -out > ./{output_file} 2>/dev/null")!=0):
         print("Clipboard does not contain an image")
