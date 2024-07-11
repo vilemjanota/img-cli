da62bb40e55bba59c380a26df1fb88ae695cf4d4
Vilém Janota <willjanota@gmail.com>
2024-07-11 23:20:00 +0200

    add resize options

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -7,7 +7,10 @@ def print_usage():
     """
     print("Usage: img-cli <command> <options>")
     print("Commands:")
-    print("save <filename> - Save an image from clipboard")
+    print("save <options> <filename> - Save an image from clipboard")
+    print("\tOptions:")
+    print("\t-h <height> - Resize the image to a specific height")
+    print("\t-w <width> - Resize the image to a specific width")
 
 def main():
     """
@@ -22,7 +25,29 @@ def main():
         if len(args) < 2:
             print("Error: save command requires a filename")
             return
-        save(args[1])
+        if len(args) == 2:
+            save(args[1], 'original', 'original')
+            return
+        else:
+            height = 'original'
+            width = 'original'
+            # Check if -h flag is present and if the next argument is a valid number
+            if "-h" in args and args.index("-h") != len(args)-1 and args[args.index("-h")+1].isalnum():
+                height = args[args.index("-h")+1]
+                args.pop(args.index("-h")+1)
+                args.remove("-h")
+                
+            # Check if -w flag is present and if the next argument is a valid number
+            if "-w" in args and args.index("-w") != len(args)-1 and args[args.index("-w")+1].isalnum():
+                width = args[args.index("-w")+1]
+                args.pop(args.index("-w")+1)
+                args.remove("-w")
+            save(args[1], height, width)
+            return
+
+
+    print("Error: unknown command")
+    print_usage()
 
 if __name__ == '__main__':
     main()
--- pycli/save.py
+++ pycli/save.py
@@ -1,6 +1,6 @@
 import os
 
-def save(output_file):
+def save(output_file, height, width):
     """
     Save an image from the clipboard to a file.
 
@@ -11,6 +11,17 @@ def save(output_file):
         None
     """
     print("saving to", output_file + "...")
-    if(os.system(f"xclip -selection clipboard -target image/png -out > ./{output_file} 2>/dev/null")!=0):
-        print("Clipboard does not contain an image")
+    os.system(f"xclip -selection clipboard -target image/png -out > ./{output_file} 2>error.txt")
+    if (os.path.exists('error.txt') and os.path.getsize('error.txt') > 0):
+        with open('error.txt', 'r') as file:
+            error_message = file.read()
+        print("Error occurred:", error_message)
         os.remove(output_file)
+        #os.remove('error.txt')
+        return
+    if(height != 'original' and width != 'original'):
+        os.system(f"convert {output_file} -resize {width}x{height} {output_file}")
+    elif(height != 'original'):
+        os.system(f"convert {output_file} -resize x{height} {output_file}")
+    elif(width != 'original'):
+        os.system(f"convert {output_file} -resize {width}x {output_file}")
